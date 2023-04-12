5c863f937413f5628849d19af9d838ea01ed55fe
wolfkang <hungrywolfkang@gmail.com>
2023-04-12 10:26:50 -0400

    viaualizer file upload

---
+++ ballsWarpVisualizer.js
@@ -0,0 +1,121 @@
+import * as THREE from './js/three.module.js';
+
+export function ballsWarpVisualizer(scene, camera, renderer, dataArray, analyser){
+   
+     const geometry = new THREE.SphereGeometry(1, 10, 10);
+     const material1 = new THREE.MeshStandardMaterial({
+          //color: 0x05f0c1, 
+          //color: 0xff9472, 
+          color: 0xff1818, 
+          roughness: 0.21, 
+          metalness: 0.9,
+          //envMap: renderTarget.texture
+     });
+     const material2 = new THREE.MeshStandardMaterial({
+          //color: 0xff1a55, 
+          //color: 0xfdd400, 
+          color: 0x4d4dff, 
+          roughness: 0.21, 
+          metalness: 1, 
+          //envMap: renderTarget.texture
+     });
+
+     let balls = [];
+     let ballGroup = new THREE.Group(); 
+     const rangeMax = 32;
+     const rangeMin = -32;
+     const gap = 6;
+     let flag = true;
+     for(let x = rangeMin; x <= rangeMax; x += gap){
+          for(let y = rangeMin; y <= rangeMax; y += gap){
+               for(let z = rangeMin * 10; z <= rangeMax; z += gap){
+                    flag = !flag;
+                    const mesh = new THREE.Mesh(geometry, flag ? material1 : material2);
+                    
+                    mesh.position.set(x, y, z);
+                    ballGroup.add(mesh);
+                    balls.push(mesh);
+                    //scene.add(ballGroup);
+                    //console.log(balls);
+               }
+          }
+          
+     }
+     scene.add(ballGroup);
+
+     let tl = gsap.timeline({repeat: 3, repeatDelay: 4});
+     tl.to(camera.position, {z: 80, duration:12});
+     tl.to(camera.position, {z: -80, duration:16});
+     tl.to(camera.rotation, {z: Math.PI, duration: 12});
+     tl.to(camera.position, {x: 40, duration:18});
+     tl.to(camera.position, {z: -50, duration:16});
+     tl.to(camera.rotation, {y: Math.PI * 2, duration: 18});
+     tl.to(camera.position, {x: 10, duration:8}); 
+     tl.to(camera.position, {y: 10, duration:8}); 
+     tl.to(camera.rotation, {z: Math.PI, duration:16}); 
+     tl.to(camera.position, {z: -225, duration:16});  
+
+     console.log(balls);
+     console.log(ballGroup);
+     function renderFrame(){ 
+          //ballGroup.rotation.z += 0.01;     
+          analyser.getByteFrequencyData(dataArray);
+         
+          for(let i = 0; i < balls.length; i++){
+               let num = Math.floor(i % 256);
+               const pitch = dataArray[num];
+
+               const pos = balls[i].position;
+               
+               //effect 0 just flycamera move effect //
+
+               // effect 1 //
+              // gsap.to(ballGroup.scale, {z: pitch/24, duration: 0.5, ease: 'Power2.easeIn'});
+               
+               // effect 2
+              // gsap.to(ballGroup.scale, {y: pitch/12, duration: 0.5, ease: 'Power2.easeIn'});     
+               
+               // effect 3
+              // gsap.to(ballGroup.scale, {x: pitch/6, duration: 0.5, ease: 'elastic'});               
+             
+             // effect 4 //
+              //gsap.to(balls[i].position, {y: pitch/6, duration: 0.3, ease: 'Power2.easeOut'}); 
+             
+             // effect5  good like vertical light show  
+               /*  gsap.to(balls[i].scale, {y: pitch/12, duration: 0.5, ease: 'elastic'});
+              gsap.to(balls[i].scale, {x: pitch/120, duration: 0.2, ease: 'elastic'});
+              gsap.to(balls[i].scale, {z: pitch/120, duration: 0.8, ease: 'elastic'});    */
+             ////////////////////////////
+             //effect 6 even better center warp light show
+              gsap.to(balls[i].scale, {y: pitch/120, duration: 0.5, ease: 'elastic'});
+             gsap.to(balls[i].scale, {x: pitch/120, duration: 0.2, ease: 'elastic'});
+             gsap.to(balls[i].scale, {z: pitch/12, duration: 0.8, ease: 'elastic'});  
+
+             //effect 7
+             // gsap.to(ballGroup.position, {z: pitch * 20, duration: 0.8, ease: 'Power2.easeOut'});
+              
+          }
+
+          renderer.render(scene, camera);            
+          requestAnimationFrame(renderFrame);
+     };  
+     renderFrame();
+};
+      
+/*           const flyControls = new FlyControls(camera, renderer.domElement);
+flyControls.movementSpeed = 16;
+flyControls.rollSpeed = Math.PI / 16;
+flyControls.autoForward = false;
+flyControls.dragToLook = true;
+flyControls = flyControls;
+
+let tl = gsap.timeline({repeat: 3, repeatDelay: 5});
+tl.to(camera.position, {y: 40, duration:16});
+tl.to(camera.rotation, {z: Math.PI, duration: 12});
+tl.to(camera.position, {x: -40, duration:18});
+tl.to(camera.position, {z: -50, duration:16});
+tl.to(camera.rotation, {y: Math.PI * 2, duration: 12});
+tl.to(camera.position, {x: 50, duration:16}); 
+} */
+
+ 
---
+++ glass.js
@@ -0,0 +1,103 @@
+import * as THREE from './js/three.module.js';
+import { TeapotGeometry } from '../js/TeapotGeometry.js';
+
+
+export function glassVisualizer(scene, camera, renderer, dataArray, analyser){
+     new THREE.TextureLoader().load('./assets/textures/space-background.jpg', texture => {
+          const renderTarget = new THREE.WebGLCubeRenderTarget(texture.image.height);
+          renderTarget.fromEquirectangularTexture(renderer, texture);
+          scene.background = renderTarget.texture;
+     });
+     const renderTargetOptions = {
+          format: THREE.RGBAFormat,
+          generateMipmaps: true,
+          minFilter: THREE.LinearMipMapLinearFilter
+     } 
+     const geometry = new THREE.BoxGeometry(10, 10, 10);
+     const material = new THREE.MeshStandardMaterial({color: 0x5f5fff});
+     const cube = new THREE.Mesh(geometry, material);
+     cube.position.set(2, 55, 0);
+     scene.add(cube);
+
+     const teapotRenderTarget = new THREE.WebGLCubeRenderTarget(1024, renderTargetOptions);
+     const teapotCamera = new THREE.CubeCamera(0.1, 1000, teapotRenderTarget);
+     const teapotGeo = new TeapotGeometry(20, 24);
+     const teapotMat = new THREE.MeshPhysicalMaterial({
+          color: 0xffffff,
+          envMap: teapotRenderTarget.texture,
+          metalness: 0.1,
+          roughness : 0.05,
+          ior: 2.5,
+          thickness: 0.3,
+          transmission: 0.9,
+          side: THREE.DoubleSide
+     })
+     const teapot = new THREE.Mesh(teapotGeo, teapotMat);
+     teapot.add(teapotCamera);
+     scene.add(teapot);
+
+     const cylinderGeo = new THREE.CylinderGeometry(2, 3.5, 45, 20);
+     const cylinderMat = new THREE.MeshPhongMaterial({
+          color: 0x678fe7,
+     })
+     let cylinder;
+     let cylinders = [];
+     const cylinderPivot = new THREE.Object3D();
+     for(let degree = 0; degree < Math.PI * 2; degree += Math.PI / 6){
+           cylinder = new THREE.Mesh(cylinderGeo, cylinderMat);
+          //const rad = THREE.Math.degToRad(degree);
+          cylinder.position.set(Math.sin(degree) * 50, 0, Math.cos(degree) * 50);
+          cylinderPivot.add(cylinder);
+          cylinders.push(cylinder);
+     
+     }
+     scene.add(cylinderPivot);
+
+     const planeGeo = new THREE.CylinderGeometry(60, 60, 0.5, 50, 50);
+     const planeMat = new THREE.MeshPhongMaterial({
+          map: new THREE.TextureLoader().load('./assets/textures/2k_neptune.jpg'),
+          color: 0xeeeeee,
+          side: THREE.DoubleSide,
+     })
+     const table = new THREE.Mesh(planeGeo, planeMat);
+     //table.rotation.x = -Math.PI / 2;
+     table.position.set(0, -20, 0);
+     scene.add(table);
+     // console.log(table);
+     // console.log(cylinder);
+     // console.log(teapot);
+     
+     const clock = new THREE.Clock();
+
+     function update(time){
+          time *= 0.001;
+          const delta = clock.getDelta();
+         
+          if(cylinderPivot){
+               cylinderPivot.rotation.y = Math.sin(time * 0.5); 
+          }
+          if(teapot){
+               teapot.visible = false;
+               const teapotCamera = teapot.children[0];
+               teapotCamera.update(renderer, scene);
+               teapot.visible = true;
+          }
+     }
+
+     function animate(time){
+          analyser.getByteFrequencyData(dataArray);
+         //console.log(dataArray.length);
+          for(let i = 0; i < 12; i++){
+               cylinders[i].position.y = map(dataArray[i * 20], 0, 255, 0.001, 1) * 20;
+          }
+
+          renderer.render(scene, camera);
+          update(time);
+          requestAnimationFrame(animate);
+     };  
+     animate(0);
+}    
+     
+function map(value, start1, stop1, start2, stop2) {
+     return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2
+}
